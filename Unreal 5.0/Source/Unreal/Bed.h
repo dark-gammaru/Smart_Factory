21f23fbbbfb052b42608961dd748f7245d96d528
NaTure77 <xnaudrornfl@gmail.com>
2022-04-28 23:46:09 +0900

    침대 상호작용 작업 시도
    한번 누르면 눈꺼풀이 반 감김

---
+++ Unreal 5.0/Source/Unreal/Bed.h
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "Bed.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class UNREAL_API UBed : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UBed();
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	bool isClickedOnce;
+
+	UFUNCTION(BlueprintCallable)
+	bool IsClickedOnce();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+		
+};
